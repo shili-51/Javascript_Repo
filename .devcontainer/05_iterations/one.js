6816b44b6f6fc3023d11cedf38ba45b65057ee6c
Shilpy <79649004+shili-51@users.noreply.github.com>
2024-04-17 11:14:17 +0000

    break and continue

---
+++ .devcontainer/05_iterations/one.js
@@ -0,0 +1,20 @@
+// break - breaks out of entire loop
+
+// for(let i = 1; i <= 20; i++){
+//     if(i == 5){
+//         console.log("Detected 5")
+//         break;
+//     }
+//     console.log(`Value of i is ${i}`);
+// }
+
+
+
+// continue - skips condition once
+for(let i = 1; i <= 20; i++){
+    if(i == 5){
+        console.log("Detected 5")
+        continue;
+    }
+    console.log(`Value of i is ${i}`);
+}
