359381cd354752abf954255269f194915de44bbc
Shilpy <79649004+shili-51@users.noreply.github.com>
2024-05-20 01:24:50 +0530

    Update call.js

--- .devcontainer/10_classes_and_oop/call.js
+++ .devcontainer/10_classes_and_oop/call.js
@@ -5,6 +5,9 @@ function SetUsername(username){
 }
 
 function createUser(username, email, password){
+
+    // this.username in function SetUsername gets erased once function's execution Context get out of function call Stack
+    // so to reference the username property of already executed function - we use call keyword
     SetUsername.call(this, username)
    
     this.email = email
